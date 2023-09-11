28719e99b1384a25f3ce4e855dee60730541d86b
Michael <madetunji1376@gmail.com>
2023-09-11 14:33:55 +0100

    the functionality and design have been implemented

--- README.md
+++ README.md
@@ -1,8 +1,118 @@
-# React + Vite
+<a name="readme-top"></a>
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+<div align="center">
+  <h3><b>BUSINESS DIRECTORY</b></h3>
+</div>
 
-Currently, two official plugins are available:
+<!-- TABLE OF CONTENTS -->
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+# 📗 Table of Contents
+
+- [📖 About the Project](#about-project)
+  - [🛠 Built With](#built-with)
+    - [Key Features](#key-features)
+- [💻 Getting Started](#getting-started)
+  - [Setup](#setup)
+  - [Prerequisites](#prerequisites)
+  - [Install](#install)
+  - [API](#API)
+- [👥 Authors](#authors)
+- [⭐️ Show your support](#support)
+
+
+<!-- PROJECT DESCRIPTION -->
+
+# 📖 [Football-ticketing-App] <a name="about-project"></a>
+
+> Describe your project in 1 or 2 sentences.
+
+**[Football-ticketing-App]** An app for reserving a ticket from a collection of matches available for a football season.
+
+## 🛠 Built With <a name="built-with"></a>
+
+> Describe the tech stack and include only the relevant sections that apply to your project.
+
+<details>
+  <ul>
+    <li><a href="https://reactjs.org/">React.js</a></li>
+  </ul>
+</details>
+
+<!-- Features -->
+
+### Key Features <a name="key-features"></a>
+
+> Describe between 1-3 key features of the application.
+
+- **[Fetch business data from a provided JSON API (you can create a mock API for this task)]**
+- **[Display a search bar at the top of the page where users can enter a business name or category to filter the results]**
+- **[List the businesses, showing their names, categories, addresses, and contact information.]**
+- **[Implement real-time filtering based on the user's search query. As the user types, the list of displayed businesses should update accordingly.]**
+- **[Ensure that the application is responsive, allowing users to view and search for businesses on both desktop and mobile devices.]**
+- **[Add error handling for failed API requests]**
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+> Add a link to your deployed project.
+
+- Coming Soon
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- GETTING STARTED -->
+
+## 💻 Getting Started <a name="getting-started"></a>
+
+> Describe how a new developer could make use of your project.
+
+To get a local copy up and running, follow these steps.
+
+### Prerequisites
+
+In order to run this project you need:
+- [Node.js](https://nodejs.org/en/)
+
+### Setup
+
+Clone this repository to your desired folder by running these commands:
+
+- $ git clone https://github.com/Therealadetunji/business-directory.git
+
+### Install
+
+Install this project with:
+
+Vite 
+Npm install
+Npm run dev
+
+### MOCK API <a name="API"></a>
+
+- $ MOCK API https://0f1927a7-ccd3-467c-9337-87d326c59ce2.mock.pstmn.io/businesses
+
+
+<!-- AUTHORS -->
+
+> Mention all of the collaborators of this project.
+
+## 👥 Authors <a name="authors"></a>
+
+👤 **Michael Adeunji**
+
+- GitHub: [@Therealadetunji](https://github.com/therealadetunji)
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/adetunji-michael/)
+
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [https://github.com/Therealadetunji/business-directory/issues]
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Table } from 'react-bootstrap';
-import './App.css'
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Table } from "react-bootstrap";
+import SearchBar from "./components/searchBar";
+import "./App.css";
 
 function App() {
-
-  // create a state variable to hold the data from the mock api
-  const [businesses, setBusinesses] = useState([]);
+  const [filteredBusinesses, setFilteredBusinesses] = useState([]);
 
   // fetch data from mock api
- 
-  const business = () => { fetch('https://846333da-2b01-4f41-90b0-1c4245c09639.mock.pstmn.io/businesses')
-  .then(response => response.json())
-  // i want to console.log and see the data from the mock api
-  .then(data => 
-    //render all the data from the mock api
-    setBusinesses(
-      data.businesses.map(business => ({
-        name: business.name,
-        category: business.category,
-        address: business.address,
-        phone: business.phone
-      }))
-    ),
-    );
-  }
-    
-    useEffect(() => {
+
+  const business = () => {
+    fetch(
+      "https://0f1927a7-ccd3-467c-9337-87d326c59ce2.mock.pstmn.io/businesses"
+    )
+      .then((response) => response.json())
+      // i want to console.log and see the data from the mock api
+      .then((data) =>
+        //render all the data from the mock api
+        setFilteredBusinesses(
+          data.businesses.map((business) => ({
+            name: business.name,
+            category: business.category,
+            address: business.address,
+            phone: business.phone,
+          }))
+        )
+      ).catch((
+        error // if there is an error show the user this message
+      ) =>
+        toast.error("There was an error fetching the data. Please try again later.", {
+          position: toast.POSITION.TOP_CENTER
+        }, console.log(error)));
+  };
+
+  useEffect(() => {
+    let fetched = true; // create a variable to hold the state of the component
+    if (fetched) {
       business();
-    }, []);
+    }
+
+    // clean up
+    return () => {
+      fetched = false;
+    };
+  }, []);
 
   return (
     // fetched data from mock api is displayed here
-    
-    <div>
 
-      <h1 className="text-center mt-5 mb-5"
-      >Businesses</h1>
+    <div className="App flex flex-col justify-center items-center ">
+      <h1 className="text-center mt-3 mb-3 text-white">Businesses</h1>
+
+      <SearchBar
+        filteredBusinesses={filteredBusinesses}
+        setFilteredBusinesses={setFilteredBusinesses}
+        business={business}
+      />
+
+      <ToastContainer />
 
       <div className="container">
         <div className="row">
@@ -53,42 +76,33 @@ function App() {
               <tbody>
                 <tr>
                   <td>
-                    {businesses.map(business => (
-                      <div key={business.name}>
-                        {business.name}
-                        </div>
+                    {filteredBusinesses.map((business) => (
+                      <div key={business.name}>{business.name}</div>
                     ))}
                   </td>
                   <td>
-                    {businesses.map(business =>((
-                      <div key={business.category}>
-                        {business.category}
-                        </div>
-                    )))}
-                  </td>
-                  <td>
-                    {businesses.map(business => (
-                      <div key={business.address}>
-                        {business.address}
-                        </div>
+                    {filteredBusinesses.map((business) => (
+                      <div key={business.category}>{business.category}</div>
                     ))}
                   </td>
                   <td>
-                    {businesses.map(business => (
-                      <div key={business.phone}>
-                        {business.phone}
-                        </div>
+                    {filteredBusinesses.map((business) => (
+                      <div key={business.address}>{business.address}</div>
+                    ))}
+                  </td>
+                  <td>
+                    {filteredBusinesses.map((business) => (
+                      <div key={business.phone}>{business.phone}</div>
                     ))}
                   </td>
                 </tr>
               </tbody>
             </Table>
-            </div>
-            </div>
-            </div>
-
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
